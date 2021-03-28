0fe92209d9d78affb28249ead3a963ad2fdc05e0
Kribas Rimal <carlousrimal@gmail.com>
2021-03-28 18:35:47 +0545

    made changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shopsolutions/pages/Home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:shopsolutions/pages/login.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Login(),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -60,15 +60,113 @@ class _LoginState extends State<Login> {
 
     if(firebaseUser != null) {
       final QuerySnapshot result = await FirebaseFirestore.instance.collection("users").where("id",isEqualTo: firebaseUser.uid).get();
+      final List<DocumentSnapshot> documents = result.docs;
+      if(documents.length == 0) {
+        // insert the user to our collection
+        FirebaseFirestore.instance.collection("users").doc(firebaseUser.uid).set({
+          "id" : firebaseUser.uid,
+          "username" : firebaseUser.displayName,
+          "profilePicture" : firebaseUser.photoURL
+        });
+        await preferences.setString("id", firebaseUser.uid);
+        await preferences.setString("username", firebaseUser.displayName);
+        await preferences.setString("photoUrl", firebaseUser.photoURL);
+      }else {
+        await preferences.setString("id", documents[0]['id']);
+        await preferences.setString("username", documents[0]['username']);
+        await preferences.setString("photoUrl", documents[0]['photoUrl']);
+      }
+
+      Fluttertoast.showToast(msg: "Login was successful");
+      setState(() {
+        loading = false;
+      });
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
     }else {
-      
+      Fluttertoast.showToast(msg: "Login Failed");
     }
   }
     @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+
+      appBar: AppBar(
+
+        backgroundColor: Colors.white,
+
+        centerTitle: true,
+
+        title: new Text(
+
+          "Login",
+
+          style: TextStyle(color: Colors.red.shade900),
+
+        ),
+
+      ),
+
+      body: Stack(
+
+        children: <Widget>[
+
+          Visibility(
+
+              visible: loading ?? true,
+
+              child: Center(
+
+                child: Container(
+
+                  alignment: Alignment.center,
+
+                  color: Colors.white.withOpacity(0.9),
+
+                  child: CircularProgressIndicator(
+
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+
+                  ),
+
+                ),
+
+              ))
+
+        ],
+
+      ),
+
+      bottomNavigationBar: Container(
+
+        child: Padding(
+
+          padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 8.0, bottom: 8.0),
+
+          child: FlatButton(
+
+              color: Colors.red,
+
+              onPressed: () {
+
+                handleSignIn();
+
+              },
+
+              child: Text(
+
+                "Sign in / Sign up with google",
+
+                style: TextStyle(color: Colors.white),
+
+              )),
+
+        ),
+
+      ),
+
+    );
+
+    }
   }
 
 
-
-}
