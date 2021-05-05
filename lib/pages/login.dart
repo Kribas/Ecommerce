19f02ca213723060af3e309f3377e83d7be822fd
Kribas Rimal <carlousrimal@gmail.com>
2021-05-05 14:55:25 +0545

    made changes

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -17,6 +17,9 @@ class _LoginState extends State<Login> {
   TextEditingController _email = TextEditingController();
   TextEditingController _password = TextEditingController();
 
+  bool hidePass = true;
+
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context);
@@ -63,14 +66,16 @@ class _LoginState extends State<Login> {
                             elevation: 0.0,
                             child: Padding(
                               padding: const EdgeInsets.only(left: 12.0),
-                              child: TextFormField(
-                                controller: _email,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "Email",
-                                  icon: Icon(Icons.alternate_email),
+                              child: ListTile(
+                                title: TextFormField(
+                                  controller: _email,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Email",
+                                    icon: Icon(Icons.alternate_email),
+                                  ),
+                                  validator: (value) => !value.contains('@') ? 'Enter a valid email' : null,
                                 ),
-                                validator: (value) => !value.contains('@') ? 'Enter a valid email' : null,
                               ),
                             ),
                           ),
@@ -84,21 +89,31 @@ class _LoginState extends State<Login> {
                             elevation: 0.0,
                             child: Padding(
                               padding: const EdgeInsets.only(left: 12.0),
-                              child: TextFormField(
-                                controller: _password,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "Password",
-                                  icon: Icon(Icons.lock_outline),
+                              child: ListTile(
+                                title: TextFormField(
+                                  obscureText: hidePass,
+                                  controller: _password,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    icon: Icon(Icons.lock_outline),
+                                  ),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return "The password field cannot be empty";
+                                    } else if (value.length < 6) {
+                                      return "the password has to be at least 6 characters long";
+                                    }
+                                    return null;
+                                  },
                                 ),
-                                validator: (value) {
-                                  if (value.isEmpty) {
-                                    return "The password field cannot be empty";
-                                  } else if (value.length < 6) {
-                                    return "the password has to be at least 6 characters long";
-                                  }
-                                  return null;
-                                },
+                                trailing: IconButton(
+                                    icon: Icon(Icons.remove_red_eye),
+                                    onPressed: () {
+                                      setState(() {
+                                        hidePass = !hidePass;
+                                      });
+                                    }),
                               ),
                             ),
                           ),
@@ -131,17 +146,7 @@ class _LoginState extends State<Login> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                "Forgot password",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: black,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              ),
-                            ),
+
                             Padding(
                                 padding: const EdgeInsets.all(8.0),
                                 child: InkWell(
